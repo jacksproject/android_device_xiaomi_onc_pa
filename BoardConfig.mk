639ca7eb2eaa384168b667f3acba9b4b3be89591
Alone0316 <rutujamandlik06@gmail.com>
2022-02-10 16:43:28 +0000

    mido: Define vendor.qti.hardware.radio.qtiradio and vendor.qti.hardware.radio.qcrilhook@1.0 in device_framework_compatibility_matrix.xml

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,9 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # HIDL
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(DEVICE_PATH)/framework_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+       $(DEVICE_PATH)/device_framework_compatibility_matrix.xml \
+       vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 
