b8f0d604c2a62783eaa84f19c1818587100c3c7c
me-cafebabe <me_cafebabe@hotmail.com>
2023-01-10 01:32:50 +0800

    mido: Inherit 2048 dalvik config as fallback for GSIs

--- device.mk
+++ device.mk
@@ -20,6 +20,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 # Apex
 PRODUCT_COMPRESSED_APEX := false
 
+$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+
 # Overlays
 PRODUCT_PACKAGES += \
     AOSPAMidoFrameworks \
