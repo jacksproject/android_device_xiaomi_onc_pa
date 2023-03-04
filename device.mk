2b3dd7b4185b588e0ab19831894745c6e6a703d3
Alone0316 <rutujamandlik06@gmail.com>
2023-03-04 08:59:10 +0000

    mido: Switch to prebuilt vendor.qti.hardware.perf blobs - From LA.UM.10.6.2.r1-01600-89xx.0 - Drop device.mk entry to build from source

--- device.mk
+++ device.mk
@@ -296,10 +296,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.0 \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
