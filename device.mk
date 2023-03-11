12c6675b4d9524082d6147c9c01bd297775c87fe
Alone0316 <rutujamandlik06@gmail.com>
2023-03-11 09:38:28 +0000

    mido: Dont build libqti_vndfwk_detect * already included in vendor/aospa

--- device.mk
+++ device.mk
@@ -206,13 +206,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libqcomfm_jni
 
-# Fwk-detect
-PRODUCT_PACKAGES += \
-    libqti_vndfwk_detect \
-    libqti_vndfwk_detect.vendor \
-    libvndfwk_detect_jni.qti \
-    libvndfwk_detect_jni.qti.vendor
-
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl:64 \
